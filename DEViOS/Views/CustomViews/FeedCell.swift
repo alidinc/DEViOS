9c06bf33bd951e771d3c992a07447369c059e39c
Ali Dinç <alidinc@Alis-MacBook-Air-M1.local>
2021-09-22 23:52:31 +0100

    .plist fixed

---
+++ DEViOS/Views/CustomViews/FeedCell.swift
@@ -0,0 +1,30 @@
+//
+//  FeedCell.swift
+//  DEViOS
+//
+//  Created by Ali Dinç on 20/09/2021.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    
+    // MARK: - Outlets
+    
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+    // MARK: - <#Description#>
+    
+}
